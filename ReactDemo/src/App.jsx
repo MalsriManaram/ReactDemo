85d355dfa7b23b9e0e0e6b7d4fb0a297c87d5cf6
MalsriManaram <malsrimanaram7878@gmail.com>
2025-02-18 12:17:05 +0530

    initial commit

---
+++ ReactDemo/src/App.jsx
@@ -0,0 +1,17 @@
+
+
+function App() {
+
+
+  return (
+    <>
+    <div className="text-left">
+      <h1 className="p-5 text-4xl font-bold">Demo React project</h1>
+      <p className="px-5 text-2xl "> Vite + Shadcn + Tailwind css</p>
+    </div>
+
+    </>
+  );
+};
+
+export default App
