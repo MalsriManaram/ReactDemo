d28d3457232ee0c503a7b349e363dd86bc1ec3e3
MalsriManaram <malsrimanaram7878@gmail.com>
2025-02-18 13:00:17 +0530

    shadcn button added

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,17 @@
+import { Button } from "./components/ui/button";
 
 
 function App() {
-
+  const ClickHandle = () => {
+    alert("Demo button clicked");
+  };
 
   return (
     <>
     <div className="text-left">
       <h1 className="p-5 text-4xl font-bold">Demo React project</h1>
       <p className="px-5 text-2xl "> Vite + Shadcn + Tailwind css</p>
+      <Button onClick={ClickHandle} className="mx-5 my-2"> Demo</Button>
     </div>
 
     </>
